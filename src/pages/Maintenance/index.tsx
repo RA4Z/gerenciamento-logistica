42ab3664e15e1821366ec943c9067250894cc678
Robert Aron Zimmermann <robertn@weg.net>
2024-09-03 15:22:21 -0300

    Criando notificação alertando erros

--- src/pages/Maintenance/index.tsx
+++ src/pages/Maintenance/index.tsx
@@ -1,19 +1,64 @@
 import { useNavigate } from 'react-router-dom';
 import styles from './Maintenance.module.scss'
-import { Input, Button, DatePicker } from 'antd';
-import { useEffect, useState } from 'react';
+import { Input, Button, notification, DatePicker } from 'antd';
+import { useState } from 'react';
 import dayjs from 'dayjs'
+import { saveData } from 'services/firestore';
 
 export default function Maintenance() {
   const [dados, setDados] = useState({ data: '', numeroEmpilhadeira: '', nomeSolicitante: '', problema: '', turno: '' })
   const navigate = useNavigate()
+  const [api, contextHolder] = notification.useNotification();
 
-  useEffect(() => {
-    console.log(dados)
-  }, [dados])
+  const openNotificationWithIcon = (type: 'error', texto: string) => {
+    api[type]({
+      message: 'Erro ao Salvar Dados',
+      description: texto,
+    });
+  };
+
+  async function saveFiles() {
+    // Validação dos campos
+    if (!dados.data) {
+      openNotificationWithIcon('error', 'Por favor, preencha o campo de data.');
+      return;
+    }
+    if (!dados.numeroEmpilhadeira) {
+      openNotificationWithIcon('error', 'Por favor, preencha o campo de número da empilhadeira.');
+      return;
+    }
+    if (!dados.nomeSolicitante) {
+      openNotificationWithIcon('error', 'Por favor, preencha o campo de nome do solicitante.');
+      return;
+    }
+    if (!dados.problema) {
+      openNotificationWithIcon('error', 'Por favor, preencha o campo de problema.');
+      return;
+    }
+    if (!dados.turno) {
+      openNotificationWithIcon('error', 'Por favor, preencha o campo de turno.');
+      return;
+    }
+
+    try {
+      // Salva os dados no Firestore
+      const result = await saveData(dados, 'maintenance');
+      if (result === 'erro') {
+        openNotificationWithIcon('error', 'Ocorreu algum erro ao salvar os dados. Por favor, tente novamente.');
+        return;
+      }
+      // Se a operação foi bem-sucedida, redireciona para a página inicial
+      navigate('/');
+    } catch (error) {
+      // Se ocorrer algum erro durante o salvamento, exibe uma mensagem de erro
+      openNotificationWithIcon('error', 'Ocorreu um erro ao salvar os dados.');
+      console.error('Erro ao salvar dados:', error);
+    }
+  }
 
   return (
     <div className={styles.container}>
+      {contextHolder}
       <h1>Maintenance</h1>
 
       <DatePicker
@@ -25,7 +70,7 @@ export default function Maintenance() {
           } else {
             setDados({ ...dados, data: dayjs(e).format('YYYY-MM-DD').toString() });
           }
-        }} 
+        }}
         placeholder='Data' />
       <Input value={dados.numeroEmpilhadeira} onChange={e => setDados({ ...dados, numeroEmpilhadeira: e.target.value })} placeholder="Número da Empilhadeira" />
       <Input value={dados.nomeSolicitante} onChange={e => setDados({ ...dados, nomeSolicitante: e.target.value })} placeholder="Nome do Solicitante" />
@@ -34,7 +79,7 @@ export default function Maintenance() {
 
       <div className={styles.buttons}>
         <Button onClick={() => navigate('/')}>Cancelar</Button>
-        <Button type="primary">Enviar</Button>
+        <Button type="primary" onClick={() => saveFiles()}>Enviar</Button>
       </div>
     </div>
   );
