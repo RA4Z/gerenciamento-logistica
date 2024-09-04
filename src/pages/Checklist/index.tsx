31c05e65eaa5228d9f361ae8012acd50e59c0d37
Robert Aron Zimmermann <robertn@weg.net>
2024-09-04 08:44:51 -0300

    Adicionando altura relativa no formulário de checklist

--- src/pages/Checklist/index.tsx
+++ src/pages/Checklist/index.tsx
@@ -1,13 +1,11 @@
+import styles from './Checklist.module.scss'
 export default function Checklist(): JSX.Element {
   return (
-    <div>
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSdXAB1XcDShp1EQdIEJjfWf_NluBk6SWpawzMOLUntJNrXr6Q/viewform?embedded=true"
-        width="100%"
-        height="5729px"
-        title="Formulário de Checklist">
-        Carregando…
-      </iframe>
-    </div>
+    <iframe
+      className={styles.container}
+      src="https://docs.google.com/forms/d/e/1FAIpQLSdXAB1XcDShp1EQdIEJjfWf_NluBk6SWpawzMOLUntJNrXr6Q/viewform?embedded=true"
+      title="Formulário de Checklist">
+      Carregando…
+    </iframe>
   );
 }
